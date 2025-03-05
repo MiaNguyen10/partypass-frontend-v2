2644e823569626923f47ea87b5be4770e7717e4c
MiaNguyen10 <hanguyendahlia@gmail.com>
2025-03-05 21:58:39 +0200

    fix phone reg

--- src/views/institution/schemaInstitution.js
+++ src/views/institution/schemaInstitution.js
@@ -1,10 +1,10 @@
 import * as yup from "yup";
-import { emailRegExp, phoneRegExp } from "../../config/regexFormat";
+import { emailRegExp } from "../../config/regexFormat";
 
 const schemaInstitution = yup.object().shape({
   name: yup.string().required("Name is required"),
   email: yup.string().matches(emailRegExp, "Email is not valid").required("Email is required"),
-  phone: yup.string().matches(phoneRegExp, "Phone is not valid").required("Phone is required"),
+  phone: yup.string().required("Phone is required"),
   address: yup.string().required("Address is required"),
   map_location: yup.string(),
   details: yup.string(),
